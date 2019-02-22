72231311f4b3cb0915191c15b46cc243831b2742
Steven Rachman <steven.rachman@gmail.com>
2019-02-22 12:07:11 +0800

    Add docs

--- src/Player.h
+++ src/Player.h
@@ -6,15 +6,34 @@
 
 #define MAX_SEQUENCE 5
 
+/**
+ * Static class to handle Sequences.
+ */
 class Player
 {
 public:
+    /**
+     * Creates a new Sequence and adds Sequence to internal list.
+     * Returns the created Sequence.
+     */
     static Sequence & newSequence();
+
+    /**
+     * Adds Sequence to internal list.
+     * All sequences in list will be updated when Player::update() is called.
+     */
     static void add(Sequence *sequence);
+
+    /** 
+     * Updates all added sequences.
+     * Put in loop().
+     */
     static void update();
 
 private:
+    // Internal list
     static Sequence *sequences[];
+
     static uint8_t length;
 };
 
--- src/Sequence.h
+++ src/Sequence.h
@@ -4,13 +4,36 @@
 #include <cstdint>
 #include "effects/Effect.h"
 
+/**
+ * Stores sequences of Effect as a linked list.
+ */
 class Sequence
 {
 public:
     // For constructing the sequence
+
+    /**
+     * Initialize Sequence, must be called first
+     * then call other constructor functions.
+     */
     Sequence & first(Effect *effect);
+
+    /**
+     * Appends Effect to the linked list.
+     * Increments length of linked list.
+     */
     Sequence & next(Effect *effect);
+
+    /**
+     * Sets the next reference of the last element
+     * without incrementing the length.
+     */
     Sequence & loopTo(Effect *effect);
+
+    /**
+     * Sets the next reference of the last element
+     * back to the first element.
+     */
     Sequence & loopToFirst();
 
     bool is_stop = true;
@@ -21,6 +44,7 @@ public:
     // To be called by Player every loop
     void update();
 
+    // Playback functions
     void play();
     void stop();
     void skip();
